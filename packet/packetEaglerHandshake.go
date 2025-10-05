4f50289e216ab6d3e3baaac14793be70ab86d32f
BrownNPC <deaddogfuneral@gmail.com>
2025-10-06 01:31:15 +0500

    checkpoint

--- packet/packetEaglerHandshake.go
+++ packet/packetEaglerHandshake.go
@@ -1,9 +1,21 @@
 package packet
 
+import "bytes"
+
 type EaglerHandshakeRequestPacket struct {
 	PacketId                             byte
 	EaglerCraftVersion, MinecraftVersion byte
 	ClientBrand                          string
 	ClientVersionString                  string
 }
-func DecodeEagler
+
+func DecodeEaglerHandshakeRequestPacket(b []byte) (EaglerHandshakeRequestPacket, error) {
+	var buf = bytes.NewBuffer(b)
+	packetId, _ := buf.ReadByte()
+	eaglerCraftVersion, _ := buf.ReadByte()
+	minecraftVersion, _ := buf.ReadByte()
+	clientBrandStrLength, _ := buf.ReadByte()
+	clientBrand := buf.Next(int(clientBrandStrLength))
+	clientVersionStrLength, _ := buf.ReadByte()
+	clientVersionString := buf.Next(int(clientVersionStrLength))
+}
